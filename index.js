c4610ee5eb7495924f965c11270c4ebf7b5b3c09
partycrusher546 <muthonidennis546@gmail.com>
2024-04-14 00:46:24 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+
+let articleHearts = document.querySelectorAll(".like-glyph");
+
+function mimicServerCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Pretend remote server notified of action!");
+    }, 300);
+  });
+}
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", function () {
+    console.log("You clicked the heart!");
+  });
+}
